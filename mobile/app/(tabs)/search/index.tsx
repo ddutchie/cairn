43d167c2955f3075a37a070f802395645dbe63a5
Gerard Slee <ddutchie@gmail.com>
2026-07-11 13:58:01 -0400

    polish(mobile): remove Search header title

--- mobile/app/(tabs)/search/index.tsx
+++ mobile/app/(tabs)/search/index.tsx
@@ -269,7 +269,7 @@ export default function SearchScreen() {
     <TabScreen>
       <Stack.Screen
         options={{
-          title: "Search",
+          title: "",
           headerSearchBarOptions: {
             ref: searchRef,
             placeholder,
